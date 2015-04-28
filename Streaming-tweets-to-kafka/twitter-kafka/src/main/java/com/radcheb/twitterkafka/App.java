91a439171d1d7fc5d4e04c487869369fd686579e
sebobr <seb.breton.0@gmail.com>
2015-04-28 11:52:44 +0200

    ds

---
+++ Streaming-tweets-to-kafka/twitter-kafka/src/main/java/com/radcheb/twitterkafka/App.java
@@ -0,0 +1,14 @@
+package com.radcheb.twitterkafka;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
