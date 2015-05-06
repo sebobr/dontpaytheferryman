c6c938830263ef69d753ebbca2cf2da5c1754d22
sebobr <seb.breton.0@gmail.com>
2015-05-06 14:19:50 +0200

    Update KafkaThreadProducer.java

--- kafka-producer/producer/src/main/java/com/core/KafkaThreadProducer.java
+++ kafka-producer/producer/src/main/java/com/core/KafkaThreadProducer.java
@@ -35,7 +35,7 @@ public class KafkaThreadProducer {
 			LOGGER.debug("Setting up file reader");
 			BufferedTwitterReader reader = new BufferedTwitterReader( input);
 			LOGGER.debug("Setting up kafka producer");
-			KafkaParams confparams = new KafkaParams(confFile)
+			KafkaParams confparams = new KafkaParams(confFile);
 			KafkaProducer kafkaProducer = new KafkaProducer(confparams, send);
 
 			LOGGER.debug("Spinning up threads");
