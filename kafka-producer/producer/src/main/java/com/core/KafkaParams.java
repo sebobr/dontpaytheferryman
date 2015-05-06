858553d1f288cdc70b6a28b58a48c0c6dc630e09
sebobr <seb.breton.0@gmail.com>
2015-05-06 10:19:31 +0200

    Update KafkaParams.java

--- kafka-producer/producer/src/main/java/com/core/KafkaParams.java
+++ kafka-producer/producer/src/main/java/com/core/KafkaParams.java
@@ -6,15 +6,14 @@ import java.util.Properties;
 
 public class KafkaParams {
 
-	public static final String BROKER_LIST = "metadata.broker.list";
-	public static final String SERIALIZER = "serializer.class";
-	public static final String REQUIRED_ACKS = "request.required.acks";
-	public static final String KAFKA_TOPIC = "kafka.topic";
-
-	public static final String BROKER_LIST_VALUE = "172.17.42.1:49154";
-	public static final String SERIALIZER_VALUE = "kafka.serializer.StringEncoder";
-	public static final String REQUIRED_ACKS_VALUE = "request.required.acks";
-	public static final String KAFKA_TOPIC_VALUE = "tweets";
+	public String BROKER_LIST = "metadata.broker.list";
+	public String SERIALIZER = "serializer.class";
+	public String REQUIRED_ACKS = "request.required.acks";
+	public String KAFKA_TOPIC = "kafka.topic";
+	public String BROKER_LIST_VALUE = "172.17.42.1:49154";
+	public String SERIALIZER_VALUE = "kafka.serializer.StringEncoder";
+	public String REQUIRED_ACKS_VALUE = "request.required.acks";
+	public String KAFKA_TOPIC_VALUE = "tweets";
 	
 	public KafkaParams(String confFile) {
 		prop = new Properties();
@@ -26,7 +25,10 @@ public class KafkaParams {
 	}
 	
 	
-	
+	public String getString(String key) {
+		return prop.getProperty(key);
+	}
+}
 	
 	
 }
