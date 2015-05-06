5df28587c88b1412eda8d06701f62f42b05d99b7
sebobr <seb.breton.0@gmail.com>
2015-05-06 10:58:56 +0200

    Update KafkaParams.java

--- kafka-producer/producer/src/main/java/com/core/KafkaParams.java
+++ kafka-producer/producer/src/main/java/com/core/KafkaParams.java
@@ -6,14 +6,14 @@ import java.util.Properties;
 
 public class KafkaParams {
 
-	public String BROKER_LIST = "metadata.broker.list";
-	public String SERIALIZER = "serializer.class";
-	public String REQUIRED_ACKS = "request.required.acks";
-	public String KAFKA_TOPIC = "kafka.topic";
-	public String BROKER_LIST_VALUE = "172.17.42.1:49154";
-	public String SERIALIZER_VALUE = "kafka.serializer.StringEncoder";
-	public String REQUIRED_ACKS_VALUE = "request.required.acks";
-	public String KAFKA_TOPIC_VALUE = "tweets";
+	public String BROKER_LIST = "broker_list";
+	public String SERIALIZER = "serializer";
+	public String REQUIRED_ACKS = "required_acks";
+	public String KAFKA_TOPIC = "kafka_topic";
+	public String BROKER_LIST_VALUE = "broker_list_value";
+	public String SERIALIZER_VALUE = "serializer_value";
+	public String REQUIRED_ACKS_VALUE = "required_acks_value";
+	public String KAFKA_TOPIC_VALUE = "kafka_topic_value";
 	
 	public KafkaParams(String confFile) {
 		prop = new Properties();
