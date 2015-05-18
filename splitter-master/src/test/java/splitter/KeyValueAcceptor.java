4c2fd2515604026cfd3c265ae5b9150b6a31821f
sebobr <seb.breton.0@gmail.com>
2015-05-18 13:20:13 +0200

    iasasa

--- splitter-master/src/test/java/splitter/KeyValueAcceptor.java
+++
@@ -1,8 +0,0 @@
-package splitter;
-
-import java.io.IOException;
-
-public interface KeyValueAcceptor<K, V> {
-	public void write(K k, V v) throws IOException, InterruptedException;
-	public void close() throws IOException, InterruptedException;
-}
