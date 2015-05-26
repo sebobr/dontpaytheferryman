432d92eec4a9bb48e13a605c3e4e6a7d77fec824
sebobr <seb.breton.0@gmail.com>
2015-05-26 14:23:13 +0200

    Update ConsumerTest.java

--- kakfa-consumer/consumer/src/main/java/com/core/ConsumerTest.java
+++ kakfa-consumer/consumer/src/main/java/com/core/ConsumerTest.java
@@ -6,16 +6,21 @@ import kafka.consumer.KafkaStream;
 public class ConsumerTest implements Runnable {
     private KafkaStream m_stream;
     private int m_threadNumber;
+    private LogWriter logw;
+    private OutputStreamWriter osw;
  
-    public ConsumerTest(KafkaStream a_stream, int a_threadNumber) {
+    public ConsumerTest(KafkaStream a_stream, int a_threadNumber, LogWriter logw) {
         m_threadNumber = a_threadNumber;
         m_stream = a_stream;
+        this.logw = logw;
+        osw = logw.getWriter();
     }
  
     public void run() {
         ConsumerIterator<byte[], byte[]> it = m_stream.iterator();
         while (it.hasNext())
-            System.out.println("Thread " + m_threadNumber + ": " + new String(it.next().message()));
+           osw.write(new String(it.next().message()));
+           // System.out.println("Thread " + m_threadNumber + ": " + new String(it.next().message()));
         System.out.println("Shutting down Thread: " + m_threadNumber);
     }
 }
