5e974125363f770765f88ad040409620ff90d7a8
sebobr <seb.breton.0@gmail.com>
2015-05-28 10:25:16 +0200

    Update ConsumerGroupExample.java

--- kakfa-consumer/consumer/src/main/java/com/core/ConsumerGroupExample.java
+++ kakfa-consumer/consumer/src/main/java/com/core/ConsumerGroupExample.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.io.OutputStreamWriter;
  
 public class ConsumerGroupExample {
     private final ConsumerConnector consumer;
